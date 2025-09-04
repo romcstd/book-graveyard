bd73f2030a271535bbb7276493cde250240edf7e
romcstd <romelroxascustodio@gmail.com>
2025-09-04 18:47:54 +0800

    release-v0.0.7

--- client/components/hero-section.tsx
+++ client/components/hero-section.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
 export function HeroSection() {
     return (
-        <section className="relative px-8 sm:px-16 py-8">
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12">
-                <div className="space-y-4 leading-relaxed text-justify tracking-wide">
+        <section className="relative px-8 sm:px-16 lg:px-32 py-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12 text-sm leading-relaxed text-muted-foreground">
+                <div className="space-y-4 leading-relaxed text-justify">
                     <div>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ipsam vel fugiat commodi quod laudantium, rerum qui consequatur cum in esse rem iste adipisci, molestiae similique culpa quam corporis odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum dolorem totam, illo ut temporibus sunt possimus quam corrupti minima praesentium facilis cumque sequi quaerat. Veniam animi ipsum autem explicabo. Voluptatem. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis iusto pariatur iste sit nihil nulla reiciendis beatae magni quo quod est quia ratione a, laborum error in, suscipit ea impedit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit cumque vitae omnis quia earum saepe impedit animi modi autem quae, nulla sed iusto inventore quos libero voluptates commodi reiciendis quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ipsum culpa dolorem eveniet quas, quaerat voluptatem mollitia, quia cumque officiis voluptatibus. Placeat illum repudiandae iusto ad ipsa corrupti temporibus illo. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis, doloribus et. Quis, illum facere! Velit rem enim excepturi, facere, ea quaerat quo commodi sit, dolorum nulla odio? Aliquam, temporibus mollitia? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum possimus rem impedit nemo harum quasi obcaecati in, voluptatibus saepe tempora, qui, veniam quis excepturi quia odio inventore at quaerat nobis! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur repellendus ad placeat totam mollitia rerum veritatis molestiae tenetur, explicabo quidem magni ullam repellat eveniet dolor! Ratione eum similique asperiores at.
                     </div>
