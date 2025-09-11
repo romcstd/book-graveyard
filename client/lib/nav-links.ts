447545ade6a6d9057671f754e8a75b397a95f6f8
romcstd <romelroxascustodio@gmail.com>
2025-09-11 23:36:50 +0800

    release-v0.1.6 | Improved menu accessibility

---
+++ client/lib/nav-links.ts
@@ -0,0 +1,10 @@
+import { NavLink } from "@/types/nav-link";
+
+export const navLinks: NavLink[] = [
+  { name: "Home", href: "/" },
+  { name: "Current Book We Are Reading", href: "/current-book-we-are-reading" },
+  { name: "Books We Read", href: "/books" },
+  { name: "About Ella", href: "/reviewers/ella-mabini" },
+  { name: "About Jamie", href: "/reviewers/jamie-rebugio" },
+  { name: "Newsletter", href: "/newsletter" },
+];
